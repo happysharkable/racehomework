eb685acea3afe8c63a10c23c33571cf24262197a
Alexander Bondarenko <happy-shark@ya.ru>
2020-03-04 23:42:20 +0300

    homework done

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
+import java.util.concurrent.CountDownLatch;
+
 public class MainClass {
-    public static final int CARS_COUNT = 4;
+    private static final int CARS_COUNT = 4;
+    private static CountDownLatch isReadyCdl = new CountDownLatch(CARS_COUNT);
+    private static CountDownLatch isFinishedCdl = new CountDownLatch(CARS_COUNT);
+
     public static void main(String[] args) {
         System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Подготовка!!!");
         Race race = new Race(new Road(60), new Tunnel(), new Road(40));
@@ -10,7 +15,31 @@ public class MainClass {
         for (int i = 0; i < cars.length; i++) {
             new Thread(cars[i]).start();
         }
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
-        System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+
+        try {
+            isReadyCdl.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка началась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            isFinishedCdl.await();
+            System.out.println("ВАЖНОЕ ОБЪЯВЛЕНИЕ >>> Гонка закончилась!!!");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static int getCarCount() {
+        return CARS_COUNT;
+    }
+
+    static CountDownLatch getReadyCountDownLatch() {
+        return MainClass.isReadyCdl;
+    }
+
+    static CountDownLatch getFinishedCountDownLatch() {
+        return MainClass.isFinishedCdl;
     }
 }
